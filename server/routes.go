932f35bb1ca9277eff64d0d30155b98fa8d86c0e
Sune Simonsen <sune@we-knowhow.dk>
2023-08-10 23:09:41 +0200

    fiddling with lazy loading

--- server/routes.go
+++ server/routes.go
@@ -1,6 +1,9 @@
 package server
 
+import "net/http"
+
 func (s *server) setupRoutes() {
 	s.router.GET("/", s.Index())
 	s.router.GET("/story/:id", s.Story())
+	s.router.ServeFiles("/assets/*filepath", http.Dir("server/assets"))
 }
