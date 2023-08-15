387d35c0dc4d36bbcaaffe36f750eb1624b1456f
Sune Simonsen <sune@we-knowhow.dk>
2023-08-15 11:14:43 +0200

    Load top stories

--- server/routes.go
+++ server/routes.go
@@ -4,6 +4,7 @@ import "net/http"
 
 func (s *server) setupRoutes() {
 	s.router.GET("/", WithErrorHandling(s.Index()))
+	s.router.GET("/topstories", WithErrorHandling(s.TopStories()))
 	s.router.GET("/story/:id", WithErrorHandling(s.Story()))
 	s.router.ServeFiles("/assets/*filepath", http.Dir("server/assets"))
 }
---
+++ server/topstories.go
@@ -0,0 +1,39 @@
+package server
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (s *server) TopStories() Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) error {
+		url := "https://hacker-news.firebaseio.com/v0/topstories.json"
+
+		storyResponse, err := http.Get(url)
+		if err != nil {
+			return err
+		}
+
+		topStoriesData, err := io.ReadAll(storyResponse.Body)
+		if err != nil && err != io.EOF {
+			return err
+		}
+
+		ids := []int{}
+		err = json.Unmarshal(topStoriesData, &ids)
+		if err != nil {
+			fmt.Println(err.Error())
+			return err
+		}
+
+		return s.templates.ExecuteTemplate(
+			w,
+			"topstories.gohtml",
+			ids,
+		)
+	}
+}
