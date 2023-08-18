59961ba68e83199428d6e5cf40fc1ad105a157e5
Sune Simonsen <sune@we-knowhow.dk>
2023-08-18 16:17:05 +0200

    Fixed pointer reciever

--- repo/story.go
+++ repo/story.go
@@ -6,7 +6,7 @@ import (
 	"github.com/sunesimonsen/htmx-hackernews/model"
 )
 
-func (host *Host) GetStory(id string) (model.Story, error) {
+func (host Host) GetStory(id string) (model.Story, error) {
 	story := model.Story{}
 	err := host.LoadJson(fmt.Sprintf("/v0/item/%s.json", id), &story)
 
