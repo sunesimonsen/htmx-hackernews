afda58de3ce36f9b386d39bdc5d13b485de16e67
Sune Simonsen <sune@we-knowhow.dk>
2023-10-23 07:50:19 +0200

    Added logging middleware

---
+++ middleware/logging.go
@@ -0,0 +1,62 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"runtime/debug"
+	"time"
+)
+
+type responseWriter struct {
+	http.ResponseWriter
+	status      int
+	wroteHeader bool
+}
+
+func wrapResponseWriter(w http.ResponseWriter) *responseWriter {
+	return &responseWriter{ResponseWriter: w, status: 200}
+}
+
+func (rw *responseWriter) Status() int {
+	return rw.status
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	if rw.wroteHeader {
+		return
+	}
+
+	rw.status = code
+	rw.ResponseWriter.WriteHeader(code)
+	rw.wroteHeader = true
+
+	return
+}
+
+func Logging(logger *log.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			defer func() {
+				if err := recover(); err != nil {
+					w.WriteHeader(http.StatusInternalServerError)
+					logger.Println(
+						"err", err,
+						"trace", debug.Stack(),
+					)
+				}
+			}()
+
+			start := time.Now()
+			wrapped := wrapResponseWriter(w)
+			next.ServeHTTP(wrapped, r)
+			logger.Println(
+				"status", wrapped.status,
+				"method", r.Method,
+				"path", r.URL.EscapedPath(),
+				"duration", time.Since(start),
+			)
+		}
+
+		return http.HandlerFunc(fn)
+	}
+}
--- server/routes.go
+++ server/routes.go
@@ -7,21 +7,17 @@ import (
 )
 
 func (s *server) setupRoutes() {
-	s.router.GET("/", view.WithView(
-		s.templates,
-		view.IndexView{},
-	))
-	s.router.GET("/topstories", view.WithView(
-		s.templates,
-		view.TopStoriesView{Repo: s.repo},
-	))
-	s.router.GET("/story/:id", view.WithView(
-		s.templates,
-		view.StoryView{Repo: s.repo},
-	))
-	s.router.GET("/comment/:id", view.WithView(
-		s.templates,
-		view.CommentView{Repo: s.repo},
-	))
+	s.router.GET("/",
+		view.WithView(s.templates, view.IndexView{}),
+	)
+	s.router.GET("/topstories",
+		view.WithView(s.templates, view.TopStoriesView{Repo: s.repo}),
+	)
+	s.router.GET("/story/:id",
+		view.WithView(s.templates, view.StoryView{Repo: s.repo}),
+	)
+	s.router.GET("/comment/:id",
+		view.WithView(s.templates, view.CommentView{Repo: s.repo}),
+	)
 	s.router.ServeFiles("/assets/*filepath", http.Dir("server/assets"))
 }
